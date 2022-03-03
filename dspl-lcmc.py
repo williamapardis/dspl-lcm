74c3bf8135de1892478b016fd7612605545f6beb
William Pardis <wpardis@whoi.edu>
2022-03-03 21:30:37 +0000

    dspl client and daemon working with lcm data type

---
+++ dspl-lcmc.py
@@ -0,0 +1,19 @@
+import lcm
+import time
+import numpy as np
+
+from lcmtypes.dspl.dspl import dspl_t
+
+lc = lcm.LCM()
+
+msg = dspl_t()
+msg.utime = int(time.time() * 1000000)
+msg.lightNumber = 1
+msg.temperature = float(np.random.uniform(4.1,33.5))
+msg.humidity = float(np.random.uniform(10.5,95.2))
+msg.channelMode = 5
+msg.lightLevel = int(np.random.uniform(0,100))
+msg.secsSinceComs = float(10)
+msg.nackCount = float(2)
+
+lc.publish("EXAMPLE", msg.encode())
---
+++ dspl-lcmd.py
@@ -0,0 +1,30 @@
+# This example demonstrates how to use LCM with the Python select module
+
+import select
+import lcm
+from lcmtypes.dspl.dspl import dspl_t
+
+def my_handler(channel, data):
+    msg = dspl_t.decode(data)
+    print("Received message on channel \"%s\"" % channel)
+    print("   utime   = %s" % str(msg.utime))
+    print("   lightNum    = %s" % str(msg.lightNumber))
+    print("   temp = %s" % str(msg.temperature))
+    print("   humidity: %s" % str(msg.humidity))
+    print("   channelMode        = '%s'" % msg.channelMode)
+    print("   lightLevel     = %s" % str(msg.lightLevel))
+    print("")
+
+lc = lcm.LCM()
+lc.subscribe("EXAMPLE", my_handler)
+
+try:
+    timeout = 1.5  # amount of time to wait, in seconds
+    while True:
+        rfds, wfds, efds = select.select([lc.fileno()], [], [], timeout)
+        if rfds:
+            lc.handle()
+        else:
+            print("Waiting for message...")
+except KeyboardInterrupt:
+    pass
