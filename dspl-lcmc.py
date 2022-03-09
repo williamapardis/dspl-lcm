ae28872afd38ab90d7cd85e79a67c7c89df6015f
William Pardis <wpardis@whoi.edu>
2022-03-09 15:50:16 -0500

    creating pyqt gui to control the LCM, still need to incorp LCM this was done on my windows so no LCM installed.

--- dspl-lcmc.py
+++ dspl-lcmc.py
@@ -1,19 +1,66 @@
-import lcm
-import time
-import numpy as np
+# import lcm
+# import time
+# import numpy as np
 
+# from lcmtypes.dspl import dspl_t
+
+# lc = lcm.LCM()
+
+# msg = dspl_t()
+# msg.utime = int(time.time() * 1000000)
+# msg.lightNumber = 1
+# msg.temperature = float(np.random.uniform(4.1,33.5))
+# msg.humidity = float(np.random.uniform(10.5,95.2))
+# msg.channelMode = 5
+# msg.lightLevel = int(np.random.uniform(0,100))
+# msg.secsSinceComs = float(10)
+# msg.nackCount = float(2)
+
+# lc.publish("EXAMPLE", msg.encode())
+
+# dependencies
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import * 
+from PyQt5.QtCore import * 
+# lcmtypes
 from lcmtypes.dspl import dspl_t
 
-lc = lcm.LCM()
 
+# application creation and layout
+app = QApplication([])
+app.setApplicationName("DSPL Control")
+window = QWidget()
+window.setGeometry(0,0,300,100)
+layout = QVBoxLayout()
 msg = dspl_t()
-msg.utime = int(time.time() * 1000000)
-msg.lightNumber = 1
-msg.temperature = float(np.random.uniform(4.1,33.5))
-msg.humidity = float(np.random.uniform(10.5,95.2))
-msg.channelMode = 5
-msg.lightLevel = int(np.random.uniform(0,100))
-msg.secsSinceComs = float(10)
-msg.nackCount = float(2)
 
-lc.publish("EXAMPLE", msg.encode())
+# button creation
+whiteB = QPushButton('White')
+redB   = QPushButton('Red')
+layout.addWidget(whiteB,1)
+layout.addWidget(redB,1)
+# button callback function
+def clicked(value):
+    msg.lightNumber = value
+    print(msg.lightNumber)
+# connect callbacks to buttons
+whiteB.clicked.connect(lambda: clicked(1))
+redB.clicked.connect(lambda: clicked(0))
+
+
+# Intensity input
+spin = QSpinBox()
+layout.addWidget(spin,1)
+spin.setValue(0)
+spin.setRange(0,100)
+spin.setAlignment(Qt.AlignCenter)
+# spinner callback function
+def valuechange(self):
+    msg.lightLevel = spin.value()
+    print(msg.lightLevel)
+# connect callback function to spinner    
+spin.valueChanged.connect(valuechange)
+
+window.setLayout(layout)
+window.show()
+app.exec()
