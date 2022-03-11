7991c8d7c540b4def67157d8bdc6e1ef6803db91
William Pardis <wpardis@whoi.edu>
2022-03-11 13:56:45 +0000

    linked client and daemon through upper lcm

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 Client and daemon LCM control of Deep Sea Power and Light [LSL-2025](https://www.deapsea.com/led-sealite/lsl-2025-multiray) for WHOI DSL's AUX/ROV Mesobot. Through dspl_t.lcm the topside control client communicates with the vehical side daemon to appropriate serial commands via raw_bytes_t.lcm to the [serial-lcm-bridge](git@github.com:whoidsl-mesobot/serial-lcm-bridge.git).  
 
 ## Required Installation
+###### Dependencies
+```
+pip install PyQt5
+```
 ###### Clone Repo
 ```
 git clone git@github.com:williamapardis/mesobot.git
--- dspl-lcmc.py
+++ dspl-lcmc.py
@@ -1,31 +1,28 @@
-# import lcm
-# import time
-# import numpy as np
 
-# from lcmtypes.dspl import dspl_t
-
-# lc = lcm.LCM()
-
-# msg = dspl_t()
-# msg.utime = int(time.time() * 1000000)
-# msg.lightNumber = 1
-# msg.temperature = float(np.random.uniform(4.1,33.5))
-# msg.humidity = float(np.random.uniform(10.5,95.2))
-# msg.channelMode = 5
-# msg.lightLevel = int(np.random.uniform(0,100))
-# msg.secsSinceComs = float(10)
-# msg.nackCount = float(2)
-
-# lc.publish("EXAMPLE", msg.encode())
 
 # dependencies
+import time
+import numpy as np
 import argparse
 # PyQt
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import * 
 from PyQt5.QtCore import * 
 # lcmtypes
+import lcm
 from lcmtypes.dspl import dspl_t
+
+
+# lcm handler
+lc = lcm.LCM()
+# lcm class
+msg = dspl_t()
+# static values of lcm
+msg.temperature = float(np.random.uniform(4.1,33.5))
+msg.humidity = float(np.random.uniform(10.5,95.2))
+msg.lightLevel = int(np.random.uniform(0,100))
+msg.secsSinceComs = float(10)
+msg.nackCount = float(2)
 
 
 # input arguments
@@ -35,17 +32,19 @@ args = parser.parse_args()
 light = args.light
 if(light=="upper"):
     print("commanding the upper light addr:002, ttyA3")
+    msg.lightNumber = 2
 if(light=="lower"):
     print("commanding the lower light addr:003, ttyA5")
+    msg.lightNumber = 3
 
 
 # application creation and layout 
 app = QApplication([])
-app.setApplicationName("DSPL "+light.upper()+" Control")
+app.setApplicationName("DSPL "+light+" Control")
 window = QWidget()
 window.setGeometry(0,0,300,100)
 layout = QVBoxLayout()
-msg = dspl_t()
+
 
 
 # button creation
@@ -55,8 +54,10 @@ layout.addWidget(whiteB,1)
 layout.addWidget(redB,1)
 # button callback function
 def clicked(value):
-    msg.lightNumber = value
-    print(msg.lightNumber)
+    msg.channelMode = value
+    msg.utime = int(time.time() * 1000000)
+    lc.publish(light, msg.encode())
+    print(msg)
 # connect callbacks to buttons
 whiteB.clicked.connect(lambda: clicked(1))
 redB.clicked.connect(lambda: clicked(0))
@@ -71,6 +72,8 @@ spin.setAlignment(Qt.AlignCenter)
 # spinner callback function
 def valuechange(self):
     msg.lightLevel = spin.value()
+    msg.utime = int(time.time() * 1000000)
+    lc.publish(light, msg.encode())
     print(msg.lightLevel)
 # connect callback function to spinner    
 spin.valueChanged.connect(valuechange)
